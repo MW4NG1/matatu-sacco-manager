5e84ad34d2b300679c5ab09b1223f54316f4d00e
MW4NG1 <msage3803@gmail.com>
2026-09-12 23:27:39 +0300

    feat: add require_admin decorator to enforce admin privileges

--- utils/decorators.py
+++ utils/decorators.py
@@ -11,3 +11,14 @@ def require_auth(func):
             return None
         return func(*args, **kwargs)
     return wrapper
+
+def require_admin(func):
+    """Decorator to ensure active user has admin privileges."""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        user = get_current_user()
+        if not user or user.get("role") != "admin":
+            print("[!] Access Denied: Admin permissions required.")
+            return None
+        return func(*args, **kwargs)
+    return wrapper
