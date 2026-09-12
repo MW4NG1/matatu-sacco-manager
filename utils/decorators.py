4c907edb64a6c894147e9d7ff1fdd32005c229dc
MW4NG1 <msage3803@gmail.com>
2026-09-12 23:27:04 +0300

    feat: add require_auth decorator to enforce user authentication

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,2 +1,13 @@
 from functools import wraps
 from utils.auth import get_current_user
+
+def require_auth(func):
+    """Decorator to ensure a user is logged in before executing a function."""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        user = get_current_user()
+        if not user:
+            print("[!] Access Denied: You must be logged in to perform this action.")
+            return None
+        return func(*args, **kwargs)
+    return wrapper
