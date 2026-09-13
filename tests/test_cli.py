9a212f11fc9a5603c8e8e7980f48b54feb0b3328
MW4NG1 <msage3803@gmail.com>
2026-09-13 23:05:40 +0300

    test: enhance exit option test with output assertions

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,3 +9,9 @@ def test_cli_exit_option(capsys):
             main.main()
         except SystemExit:
             pass
+
+    captured = capsys.readouterr()
+    assert "Goodbye" in captured.out or "Exiting" in captured.out or captured.out != ""
+
+def test_cli_invalid_menu_option(capsys):
+    user_inputs = ["99", "0"]
