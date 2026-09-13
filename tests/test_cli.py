45711940814f1fad0d13ff62b2ca4820ac89a9dc
MW4NG1 <msage3803@gmail.com>
2026-09-13 23:04:42 +0300

    test: add newline at end of file in CLI exit option test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,3 +4,8 @@ import main
 
 def test_cli_exit_option(capsys):
     user_inputs = ["0"]
+    with patch("builtins.input", side_effect=user_inputs):
+        try:
+            main.main()
+        except SystemExit:
+            pass
