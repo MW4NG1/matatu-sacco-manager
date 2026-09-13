685405c14b4989e0ad4b98b1025a6e270cf14254
MW4NG1 <msage3803@gmail.com>
2026-09-13 22:59:04 +0300

    test: add test for saving and loading JSON data

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -2,3 +2,11 @@ import os
 import json
 import pytest
 from utils.storage import load_json, save_json
+
+def test_save_and_load_json(tmp_path):
+    """Test saving data to a JSON file and reading it back."""
+    test_file = tmp_path / "test_sacco_data.json"
+    sample_data = [
+        {"id": 1, "route": "CBD - Westlands", "fare": 100},
+        {"id": 2, "route": "CBD - Karen", "fare": 150}
+    ]
