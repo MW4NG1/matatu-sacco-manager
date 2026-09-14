7a9305fdd35b27cc4a0048e35e1eb90ee8f98f48
--add <+>
2026-09-14 14:28:20 +0300

    final codes

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,3 +12,13 @@ def test_cli_exit_option(capsys):
 
     captured = capsys.readouterr()
     assert captured.out != ""
+    def test_cli_invalid_menu_option(capsys):
+     user-inputs = ["99", "5"]
+    with patch("builtins.input", side_effect=user_inputs):
+        try:
+            main.main()
+        except (SystemExit, StopIteration):
+            pass
+
+    captured = capsys.readouterr()
+    assert "Please select one of the available options" in captured.out or captured.out != ""
