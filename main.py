07e523e40f099da48a9a797cbee9c4d755d3aba6
--add <+>
2026-09-14 14:33:50 +0300

    second finished code

--- main.py
+++ main.py
@@ -20,3 +20,31 @@ def display_header():
             border_style="green"
         )
     )
+    def view_routes():
+     display_header()
+    routes = load_json("data/routes.json")
+    
+    table = Table(title="Available SACCO Routes", header_style="bold magenta")
+    table.add_column("Route ID", style="cyan")
+    table.add_column("Source")
+    table.add_column("Destination")
+    table.add_column("Fare (KES)", justify="right", style="green")
+
+    if not routes:
+        console.print("[yellow]No routes registered in the system yet.[/yellow]")
+    else:
+        for r in routes:
+            table.add_row(r.get("route_id", "-"), r.get("source", "-"), r.get("destination", "-"), f"{r.get('fare', 0):.2f}")
+        console.print(table)
+    
+    Prompt.ask("\nPress [bold]Enter[/bold] to return to menu")
+
+@require_admin
+def add_route():
+    display_header()
+    console.print("[bold yellow]Add New Route[/bold yellow]\n")
+    
+    route_id = Prompt.ask("Enter Route ID (e.g., R01)")
+    source = Prompt.ask("Enter Departure Point")
+    destination = Prompt.ask("Enter Destination")
+    fare = Prompt.ask("Enter Fare Amount (KES)")
